9193466669c73f927759fb146dfd53114ff0bded
Daniel Smith <daniel.smith@datadoghq.com>
2018-11-26 16:33:52 -0500

    Restore Snappy support
    This is enough to get `go build` and `go test` to work. From inspection, we
    also need lz4 support, but there are no tests because :shrug:.

--- cgo_flags_common.go
+++ cgo_flags_common.go
@@ -1,6 +1,6 @@
 package levigo
 
-// #cgo CFLAGS: -I${SRCDIR}/deps/lz4 -I${SRCDIR}/deps/leveldb/include -fno-builtin-memcmp -O2 -DLEVELDB_PLATFORM_POSIX -DLEVELDB_IS_BIG_ENDIAN=0
-// #cgo CXXFLAGS: -I${SRCDIR}/deps/lz4 -I${SRCDIR}/deps/leveldb/include -I${SRCDIR}/deps/leveldb -std=c++11 -fno-builtin-memcmp -O2 -lpthread -DLEVELDB_PLATFORM_POSIX -DLEVELDB_IS_BIG_ENDIAN=0
+// #cgo CFLAGS: -I${SRCDIR}/deps/lz4 -I${SRCDIR}/deps/leveldb/include -fno-builtin-memcmp -O2 -DLEVELDB_PLATFORM_POSIX -DLEVELDB_IS_BIG_ENDIAN=0 -DHAVE_SNAPPY
+// #cgo CXXFLAGS: -I${SRCDIR}/deps/lz4 -I${SRCDIR}/deps/leveldb/include -I${SRCDIR}/deps/leveldb -std=c++11 -fno-builtin-memcmp -O2 -lpthread -DLEVELDB_PLATFORM_POSIX -DLEVELDB_IS_BIG_ENDIAN=0 -DHAVE_SNAPPY
 // #include "leveldb/c.h"
 import "C"
